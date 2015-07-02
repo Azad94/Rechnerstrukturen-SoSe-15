4677cabf4a20293c180179a8176f29a9a7df14e2
Azad <sheraz.azad@stud.fh-luebeck.de>
2015-07-02 13:21:37 +0200

    Aufgabe_4_Bedarfsampel
    - Variablen müssen gesetzt werden (nacht, bedarf)
    - jederzeit Nachtmodus umschaltbar
    - Phase trotzdem durchlaufen
    - Bedarf ab Z5 wieder zurücksetzen
    -

--- Aufgabe_Bedarfsampel/Aufgabe_Bedarfsampel/Aufgabe_Bedarfsampel.asm
+++ Aufgabe_Bedarfsampel/Aufgabe_Bedarfsampel/Aufgabe_Bedarfsampel.asm
@@ -13,7 +13,9 @@
 ;		Hier nur Sprungtabelle anlegen. Mindestens für den 
 ;		Reset-Interrupt (An Adresse 0):
 	.org	0		; Am Anfang des Code-Speicher 
-	jmp start		; Sprung bei reset
+	jmp vorbereiten		; Sprung bei reset
+	.org OC1Aaddr	; Timer1 Overflow Interrupt
+	jmp OCR1A_isr	; Timer1 Overflow Interrupt
 ; hier folgen weitere Interrupts, wenn benötigt
 ;	jmp TIMER-ISR	; Sprungvektor 
 ; ***** Hier werden die "Variablen" definiert
@@ -22,19 +24,30 @@ T1count:	.byte 1	; Zähler für Wartefunktion mit Timer1
 	.cseg			; dies ist Code
 ; ----- Platz um .equ definitionen vorzunehmen
 #ifdef DEBUG 
-.equ	T1Counter = 10			; Debug-Timer-Wert	für xxs
+.equ	T1Counter = 10			; Debug-Timer-Wert für xxs
 #else
 .equ	T1Counter = 98			; Timer-Wert für 0,1s
 #endif
-.equ Timer = 5
-
-; ---------- Pin-Konfiguration -----------------------------------------------------------------
-.equ A_gruen_F_rot = 0x06	
-.equ A_gelb_F_rot = 0x0A
-.equ A_rot_F_rot = 0x12
-.equ A_rot_F_gruen = 0x11
-.equ A_rotgelb_F_rot = 0x1A
-.equ A_F_aus = 0x00
+.equ Timer=4
+; ----- Programm beginnt hinter der Vektortabelle: -----
+	.org 2*INT_VECTORS_SIZE
+; hier kommen die Interrupt Service Routinen hin, wenn benutzt
+/********************************************************/
+;	ISR Timer1 Compare Interrupt A
+OCR1A_isr:	
+	push r16				; Register auf den Stack retten
+    in   r16, SREG			; Statusregister zum Sichern nach R16
+	push r16				; und auf den Stack
+	lds r16, T1count		; Zählwert lesen
+	tst r16					; schon Null?
+	breq noDec				; ja: nichts weiter Abziehen
+	dec r16					; T1count--
+	sts T1count, r16		; und abspeichern
+noDec:
+    pop r16					; die Statusregister von Stack holen
+	out SREG, r16			; und wieder herstellen
+	pop r16					; Register restaurierten
+	reti					; Return from Interrupt;
 
 ; ----- Initialisierung von Timer1 auf 0,1s Periode -----
 InitT1:
@@ -74,51 +87,59 @@ wait:
 waitNow:
 ; polling auf Counter=0
 	lds r16, T1count
+	dec R16
 	tst r16
 	brne waitNow
 	ret
 
-; ***** Einsprungpunkt in das Hauptprogramm *****
-start:
-; ----- Initialisierung der grundlegenden Funktionen -----
-	; Initialisieren des Stacks am oberen Ende des RAM
-    ; 16 bit SP wird als SPH:SPL im IO-Space angesprochen 
-    ldi r16, LOW(RAMEND)	; low-Byte von RAMEND nach r16
+	; ---------- Pin-Konfiguration -----------------------------------------------------------------
+.equ A_gruen_F_rot = 0x06	
+.equ A_gelb_F_rot = 0x0A
+.equ A_rot_F_rot = 0x12
+.equ A_rot_F_gruen = 0x11
+.equ A_rotgelb_F_rot = 0x1A
+.equ A_F_aus = 0x00
+
+.equ nacht = 1				´;Nachtmodus AUS
+.equ bedarf = 1				 ;Bedarfsmodus AUS
+
+;	********** HAUPTPROGRAMM **********
+;	**********				 **********
+setTimer:
+		ldi R16, Timer
+		call wait 
+		ret
+
+
+; ----- auf Vorbedingung prüfen (Nachtmodus) ----- 
+vorbereiten:
+
+	ldi r16, LOW(RAMEND)	; low-Byte von RAMEND nach r16
     out SPL, r16	; in low-byte des SP ausgeben
 					; der SP liegt im IO-Space 
     ldi r16, HIGH(RAMEND)	; high-Byte von RAMEND nach r16
     out SPH, r16		; in high-byte des SP ausgeben
     ; ab hier kann der Stack verwendet werden 
+	 call InitT1
 
-	call InitT1			; Timer 1 initialisieren
-	sei					; global Interrupt enable
-	; testroutine: Lauflicht auf PortD
-	ldi r17, 0xFF		; PortD als Ausgabe
-	out DDRD, r17		; PortD als Ausgabe
-	ldi r17, 0x01		; Initialisieren des Lauflichtes
-	ldi r16, Timer		; 0,5s Wartezeit
+	ser R18
+	ser R21
+	ser R22
+	ldi R21, nacht
+	ldi R22, bedarf
+	ldi R18, 0x1F		
+	out DDRD, R18
 
+start:
 
-
-
-;	********** HAUPTPROGRAMM **********
-;	**********				 **********
-
-; ----- Ausgabe definieren -----
-	ser R16
-	ldi R16, 0x1F		
-	out DDRD, R16	
-
-; ----- auf Vorbedingung prüfen (Nachtmodus) ----- 
-vorbereiten:
-	
 	SBIC PIND, 5				; Skip Bit if Register
-	jmp Nachtmodus_an
+	dec R21
+
 
 	SBIC PIND, 6
 	jmp Nachtmodus_aus
 
-	jmp vorbereiten	
+	jmp start
 
 ;----- Zyklus für Nachtmodus AN ----
 Nachtmodus_an:	
@@ -152,32 +173,37 @@ Nachtmodus_aus:
 z2:
 	ldi R20, A_gruen_F_rot
 	out PORTD, R20
-
+	call setTimer
 z3:
 	ldi R20, A_gelb_F_rot
 	out PORTD, R20
+	call setTimer
 
 z4:
 	ldi R20, A_rot_F_rot
 	out PORTD, R20
+	call setTimer
 
 z5:
 	ldi R20, A_rot_F_gruen
 	out PORTD, R20
+	call setTimer
 	
 z6:
 	ldi R20, A_rot_F_rot
 	out PORTD, R20
+	call setTimer
 
 z7:
 	ldi R20, A_rotgelb_F_rot
 	out PORTD, R20
+	call setTimer
 
 z8:
 	ldi R20, A_gruen_F_rot
 	out PORTD, R20
+	call setTimer
 
 ;Zyklus wieder vom jeweiligen Nachtmodus-Zyklus beginnen
 back:
 	ret
-	
