5bed88635026d112b3ea4aeee0992f3f17add7b0
Sven Marquardt <Sven2q@hotmail.de>
2015-07-04 05:06:52 +0200

    Funktioniert jetzt
    Damit das ganze auch funktioniert müssen die pins ein und beim nächsten
    takt wieder aus geschlatet werden

--- Aufgabe _4/interruptWithIOHandling/interruptWithIOHandling/interruptWithIOHandling.asm
+++ Aufgabe _4/interruptWithIOHandling/interruptWithIOHandling/interruptWithIOHandling.asm
@@ -1,84 +1,67 @@
 /*
- * interruptWithIOHandling.asm
+ * INT0Demo.asm
  *
- *  Created: 03.07.2015 22:31:06
- *   Author: Sven
+ *  Created: 28.12.2012 21:33:59
+ *   Author: Ole
  */ 
- ;Die meisten Infos hab ich von hier http://www.mikrocontroller.net/articles/AVR-Tutorial:_Interrupts
- ;dieses Programm soll verdeutlichen wie in Interrupt mit einer IO zu händeln ist
- ;Wichtig zu befolgen ist ein Interrupt sollte so schnell wie möglich beendet werden es kann sonst zu einem Stau der Interrupts führen
- ;Dadurch könnten Interrupts verloren gehen oder schimmeres
- ;reti wird deshalb benutzt weil ansonsten jedes Interrupt weitere Interrupt sperrt dies wird duch reti verhindert
- ;rjmp vielleicht nciht nötig nur bei 4byte registern AUSPROBIEREN!!
-	.include  "m644PAdef.inc"
-	.org 0x000					;Steht für Reset und bedeutet start der Hardware alles wurde zurückgesetzt und programm fängt am kleinsten Bit(LBI) wieder an 
-		rjmp main
-	.org INT0addr				;Steht auch für einen festen bit wie im reset kann allerdings von Mikrocontroller zu Mikrocontroller variieren deswegen lieber die 
-		rjmp int0_handler		;begiffe verwenden das macht es leichter dieses Programm auf ander Mikrocontroller zu porten da diese begriffe durch die eingebudene Header datei 
-								;definiert sind
-	.org OC1Aaddr
-		rjmp OCR1A_isr
-	.org 3*INT_VECTORS_SIZE
-	.cseg
-/********************************************************/
-	; ISR Timer1 Compare Interrupt A
-	OCR1A_isr:
-	push r16 ; Kontext retten
-	; Code der ISR weggelassen
-	pop r16 ; Register restaurierten
-	reti ; Return from Interrupt
-
-/********************************************************/
-
-
+.include "m644PAdef.inc"	; device: ATmega 644PA
+	.cseg			; dies ist Code
+; ----- Am Beginn des Codespeichers liegt die Vektortabelle -----
+;		Hier nur Sprungtabelle anlegen. Mindestens für den 
+;		Reset-Interrupt (An Adresse 0):
+	.org	0		; Am Anfang des Code-Speicher 
+	jmp start		; Sprung bei reset
+	.org INT0addr	; Ext. INT0 Interrupt
+	jmp INT0_isr	; Ext. INT0 Interrupt
+; ----- Programm beginnt hinter der Vektortabelle: -----
+	.org 2*INT_VECTORS_SIZE
+; hier kommen die Interrupt Service Routinen hin, wenn benutzt
 /********************************************************/
 ;	ISR External Interrupt 0: INT0 
-int0_handler:	
+INT0_isr:	
 	push r16
 	push r17
-	ldi r17,0xFF			; Wert für Toggeln von Bit0
-	out PORTD, r17			; Neuen Wert ausgeben
+	ldi r17,0x01			; Wert für Toggeln von Bit0
+	in r16, PORTD			; PORT-wert lesen
+	eor r16,r17				; XOR PORTD xor 1 --> toggle
+	out PORTD, r16			; Neuen Wert ausgeben
 	pop r17					; Register restaurierten
 	pop r16					; Register restaurierten
 	reti					; Return from Interrupt;
 ;
 
-/********************************************************/
-; ----- Initialisierung des INT0 auf änderung -----
+
+; ----- Initialisierung des INT0 auf Rueckflanke -----
 InitINT0:
 	push r16
 	push r17
-	ldi r17,0x02			; Wert für (ISC01,ISC00) einstellung für rückflanke da unsere taster immer eins haben genau das was wir haben wollen
-	lds r16, EICRA			; Config EICRA in laden wichtig um die änderungen beobachten zu können im io View auf den EXTERNAL_INTERRUPT schalten dort sieht man dann die isc0 bis 2 
+	ldi r17,0x02			; Wert für (ISC01,ISC00)
+	lds r16, EICRA			; Config EICRA in laden
 	or r16,r17				; ISC01 in EICRA setzen
 	sts EICRA, r16			; und nach EICRA speichern
 	sbi EIMSK, 0			; Nur INT0 starten
 	pop r17
 	pop r16
 	ret
-/********************************************************/
 
-/*******************************************************/
-/*Hauptprogramm hier wird der Stack initalisiert und anschließend die Ausgabe der Ampel 
-bewerkstelligt. Hierhin springt das programm bei reset als erstes*/
-main:
+
+
+; ***** Einsprungpunkt in das Hauptprogramm *****
+start:
 ; ----- Initialisierung der grundlegenden Funktionen -----
-; Initialisieren des Stacks am oberen Ende des RAM
-; 16 bit SP wird als SPH:SPL im IO-Space angesprochen
-	ldi r16, LOW(RAMEND) ; low-Byte von RAMEND nach r16
-	out SPL, r16 ; in low-byte des SP ausgeben
-; der SP liegt im IO-Space
-	ldi r16, HIGH(RAMEND) ; high-Byte von RAMEND nach r16
-	out SPH, r16 ; in high-byte des SP ausgeben
-; ab hier kann der Stack verwendet werden
-	call InitINT0 ; INT0 initialisieren
-	sei
-	ldi R17, 0x03
-	out DDRD, R17
+	; Initialisieren des Stacks am oberen Ende des RAM
+    ; 16 bit SP wird als SPH:SPL im IO-Space angesprochen 
+    ldi r16, LOW(RAMEND)	; low-Byte von RAMEND nach r16
+    out SPL, r16	; in low-byte des SP ausgeben
+					; der SP liegt im IO-Space 
+    ldi r16, HIGH(RAMEND)	; high-Byte von RAMEND nach r16
+    out SPH, r16		; in high-byte des SP ausgeben
+    ; ab hier kann der Stack verwendet werden 
+
+	call InitINT0		; INT0 initialisieren
+	sei					; global Interrupt enable
+	; testroutine: PortD konfigurieren
+	ldi r17, 0x03		; PortD [1..0] als Ausgabe
+	out DDRD, r17		; PortD als Ausgabe
 stay:
-	sbi PortD, 1
-	nop
-	cbi PORTD, 1
-	nop
-	jmp stay
-/*******************************************************/	
+	jmp stay			; Endlosschleife
